a134e102b21be7caa6ed4b8a65672d6dd68e265f
Firsto <zaoroman@gmail.com>
2012-10-30 23:24:48 +0700

    HelloWorld

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Роман on 29.10.12.
+//  Copyright (c) 2012 Роман. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
